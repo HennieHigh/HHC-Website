a083eee9ffeed73fb2858527e8d3510b2b30be27
HennieHigh <henrietta.high@gmail.com>
2025-08-01 23:53:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,29 +23,3 @@ export default defineConfig(({ mode }: ConfigEnv) => ({
     },
   },
 }));
-import { defineConfig, type ConfigEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { componentTagger } from "lovable-tagger";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig(({ mode }: ConfigEnv) => ({
-  base: '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
